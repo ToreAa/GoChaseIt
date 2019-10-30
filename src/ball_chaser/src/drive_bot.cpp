91fcde12d3e5deb668d000a3a1f36c3ca91e3446
ToreAa <aarsland.tore@gmail.com>
2019-10-30 13:41:06 +0000

    worked on drive_bot

--- src/ball_chaser/src/drive_bot.cpp
+++ src/ball_chaser/src/drive_bot.cpp
@@ -21,8 +21,8 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
     // Publish the requested linear x and angular velocities to the robot wheel joints
     std_msgs::Float64 linear_x, angular_z;
 
-    linear_x_pub.publish(linear_x);
-    angular_z_pub.publish(angular.z);
+    //linear_x_pub.publish(linear_x);
+    //angular_z_pub.publish(angular.z);
     
     // Previous loop:
     // Create a motor_command object of type geometry_msgs::Twist
@@ -31,7 +31,7 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
     motor_command.linear.x = { req.linear_x, 0, 0 };
     motor_command.angular.z = { 0, 0, req.angular_z };
     // Publish angles to drive the robot
-    motor_command_publisher.publish(motor_command);
+    motor_command_pub.publish(motor_command);
 
     // Return a response message
     res.msg_feedback = "Requested wheel velocities - linear_x: " + std::to_string(linear_x) + " , angular_z: " + std::to_string(angular_z);
@@ -39,19 +39,18 @@ bool handle_drive_request(ball_chaser::DriveToTarget::Request& req,
 
 
     return true;
+}
 
 
 
-
-int main(int argc, char** argv)
-{
+int main(int argc, char **argv) {
     // Initialize a ROS node
     ros::init(argc, argv, "drive_bot");
     // Create a ROS NodeHandle object
     ros::NodeHandle n;
 
     // Inform ROS master that we will be publishing a message of type geometry_msgs::Twist on the robot actuation topic with a publishing queue size of 10
-    motor_command_publisher = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
+    ros::Publisher motor_command_pub = n.advertise<geometry_msgs::Twist>("/cmd_vel", 10);
 
     // TODO: Define a drive /ball_chaser/command_robot service with a handle_drive_request callback function
     ros::ServiceServer service = n.advertiseService("/ball_chaser/DriveToTarget", handle_drive_request);
